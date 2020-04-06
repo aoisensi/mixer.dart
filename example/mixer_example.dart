3b7efff9e469aac3cd007dcf648bde10917aa905
aoisensi <aoisensi@outlook.jp>
2020-04-06 12:38:46 +0900

    first

---
+++ example/mixer_example.dart
@@ -0,0 +1,6 @@
+import 'package:mixer/mixer.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/mixer.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library mixer;
+
+export 'src/mixer_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
